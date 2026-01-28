4373fc654f0fac8be218fbe41c1afefc2b97bc7b
hackstarjoyboy <143306287+hackstarjoyboy@users.noreply.github.com>
2026-01-28 08:40:47 +0530

    Update Axios base URL for API requests

--- movie-gold-v1/src/api/AxiosConfig.js
+++ movie-gold-v1/src/api/AxiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL:'https://ichthyological-alfredo-lutose.ngrok-free.dev',//to overcome the restriction put by the cores
-    headers:{"ngrok-skip-browser-warning":"true"}
+    baseURL:'https://springboot-api-movie-latest.onrender.com',
+   
 });
