2e18efef82e0a985c0130c7ddb8e440769cdb417
pranayborkar <143306287+hackstarjoyboy@users.noreply.github.com>
2026-05-15 07:29:26 +0530

    Update AxiosConfig.js

--- movie-gold-v1/src/api/AxiosConfig.js
+++ movie-gold-v1/src/api/AxiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL:'https://springboot-api-movie-latest.onrender.com/api/v1/movies',
+    baseURL:'https://springboot-api-movie-latest.onrender.com',
    
 });
